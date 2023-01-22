f4dd7db915ec6597677c967063508a0d5babf441
GalinaPrikhodchenko <galina.prikhodchenko@gmail.com>
2023-01-22 22:41:51 +0100

    hw 2 spaceX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 
 function App() {
+  const [launches, setLaunches] = useState([])
+  useEffect(()=> {
+    fetch('https://api.spacexdata.com/v3/launches/').then(response=>response.json()).then(resp=> setLaunches(resp))
+  }, [])
   return (
     <div>
-
+      {launches.filter(launch=> launch.launch_year !== '2020').map(launch=>{
+        return(
+            <div key={launch.flight_number} >{launch.flight_number} --- {launch.launch_year} --- {launch.mission_name} </div>
+        )
+      })}
     </div>
   );
 }
