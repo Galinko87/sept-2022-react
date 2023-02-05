e6d0335214cb76bc0cc901d7a8df4d621ac836ed
GalinaPrikhodchenko <galina.prikhodchenko@gmail.com>
2023-02-05 22:12:47 +0100

    class components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
+import {ClassComponent} from "./components/ClassComponent";
 
 
 function App() {
+    const [flag, setFlag] = useState(true);
   return (
     <div>
-
+        { flag && <ClassComponent name={'max'}/>}
+        <button onClick={()=>setFlag(prev=> !prev)}>flagButton</button>
     </div>
   );
 }
